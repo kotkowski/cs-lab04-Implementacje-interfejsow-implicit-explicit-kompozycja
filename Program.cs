fa192aefd96bfe95c87c2eafa9bac31a26968c01
Marcin Jasica <kotkowski666@gmail.com>
2020-12-15 10:21:11 +0100

    Completed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_lab04_Implementacje_interfejsow_implicit_explicit_kompozycja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
